1aa625e4c15ae1402e03dbe69c316d9ec33c9929
iammana2004 <azarnooshmana1383@gmail.com>
2024-12-19 21:48:41 +0330

    updated

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ def main():
     file_path = 'D:/meow/programm/search_al/usernames.txt'
     algorithm = Algorithm(file_path)
 
-    """sorted_file_path = 'D:/meow/programm/search_al/sorted_usernames.txt'
-    with open(sorted_file_path, 'w') as sorted_file:
-        for username in algorithm.usernames:
-            sorted_file.write(username + '\n')"""
+    #sorted_file_path = 'D:/meow/programm/search_al/sorted_usernames.txt'
+    #with open(sorted_file_path, 'w') as sorted_file:
+    #    for username in algorithm.usernames:
+    #        sorted_file.write(username + '\n')
 
     target_username = input("enter the username to search for: ")
-    #line_number1 = algorithm.linear_search(target_username)
+    line_number1 = algorithm.linear_search(target_username)
     line_number2 = algorithm.binary_search(target_username)
-    #line_number3 = algorithm.optimized_search(target_username)
+    line_number3 = algorithm.optimized_search(target_username)
 
 
-    """if line_number1 != -1:
+    if line_number1 != -1:
         print(f"Username '{target_username}' found at line {line_number1}.")
     else:
-        print(f"Username '{target_username}' not found.")"""
+        print(f"Username '{target_username}' not found.")
 
 
     if line_number2 != -1:
@@ -27,10 +27,10 @@ def main():
         print(f"Username '{target_username}' not found.")
 
 
-    """if line_number3 != -1:
+    if line_number3 != -1:
         print(f"Username '{target_username}' found at line {line_number3}.")
     else:
-        print(f"Username '{target_username}' not found.")"""
+        print(f"Username '{target_username}' not found.")
     
 
 if __name__ == "__main__":
