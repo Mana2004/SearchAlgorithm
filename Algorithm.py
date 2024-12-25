9b878a5d36c257238c763de91ad67b5e9ddb7558
iammana2004 <azarnooshmana1383@gmail.com>
2024-12-25 23:48:04 +0330

    updated Algorithm

--- Algorithm.py
+++ Algorithm.py
@@ -4,16 +4,11 @@ import time
 class Algorithm:
     def __init__(self, file_path):  
         self.file_path = file_path
-        self.bloom_size = 10000000  # Ensure this is a reasonable size for your system's memory
-        self.hash_function_count = 7
-        self.bloom_filter, self.hash_table = self._initialize_filters()
 
     def read_file(self):
         with open(self.file_path, 'r') as file:
             usernames = [line.strip() for line in file]
         return usernames
-    
-
 
     def linear_search(self, target_username):
         start_time = time.time()
@@ -25,8 +20,6 @@ class Algorithm:
             if line == target_username:
                 return line_number, f"{time.time() - start_time:.4f} seconds"
         return -1, f"{time.time() - start_time:.4f} seconds"
-
-
 
     def binary_search(self, target_username):
         start_time = time.time()
@@ -47,23 +40,15 @@ class Algorithm:
                 high = mid - 1
         return -1, f"{time.time() - start_time:.4f} seconds"
 
-
-
     def hash_table_search(self, target_username):
         start_time = time.time()
-
         usernames = self.read_file()
         hash_table = {}
         line_number = 0
         for line in usernames:
             username_hash = hashlib.sha256(line.encode()).hexdigest()
-            hash_table[username_hash] = line
+            hash_table[username_hash] = line_number 
             line_number += 1
-
-        username_hash = hashlib.sha256(target_username.encode()).hexdigest()
-        result = hash_table.get(username_hash, -1)
-        return result, f"{time.time() - start_time:.4f} seconds"
-
-
-
-
+        target_username_hash = hashlib.sha256(target_username.encode()).hexdigest()
+        line_number = hash_table.get(target_username_hash, -1)
+        return line_number, f"{time.time() - start_time:.4f} seconds"
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from Algorithm import Algorithm
+from Bloom_Filter import Bloom_Filter
 
 def main():
-    file_path = 'D:/meow/programm/search_al/firstnames.txt'
+    file_path = 'D:/meow/programm/search_al/usernames.txt'
     algorithm = Algorithm(file_path)
+    bloom = Bloom_Filter(file_path)
 
     #sorted_file_path = 'D:/meow/programm/search_al/sorted_usernames.txt'
     #with open(sorted_file_path, 'w') as sorted_file:
@@ -13,6 +15,7 @@ def main():
     line_number1 = algorithm.linear_search(target_username)
     line_number2 = algorithm.binary_search(target_username)
     line_number3 = algorithm.hash_table_search(target_username)
+    line_number4 = bloom.bloom_filter_search(target_username)
 
 
     if line_number1 != -1:
@@ -28,10 +31,15 @@ def main():
 
 
     if line_number3 != -1:
-        print(f"Username '{target_username}' optimized search {line_number3}.")
+        print(f"Username '{target_username}' hash table search {line_number3}.")
     else:
         print(f"Username '{target_username}' not found.")
     
+
+    if line_number4 != -1:
+        print(f"Username '{target_username}' bloom filter search {line_number4}.")
+    else:
+        print(f"Username '{target_username}' not found.")
 
 
 if __name__ == "__main__":
