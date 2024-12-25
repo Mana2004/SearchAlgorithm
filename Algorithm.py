59d17ac592b34781e301da65b2bb5412f3cf0235
iammana2004 <azarnooshmana1383@gmail.com>
2024-12-25 19:17:38 +0330

    added a new bloom filter

--- Algorithm.py
+++ Algorithm.py
@@ -12,22 +12,7 @@ class Algorithm:
         with open(self.file_path, 'r') as file:
             usernames = [line.strip() for line in file]
         return usernames
-
     
-    '''def _initialize_filters(self): 
-        usernames = self.read_file() 
-        hash_table = {} 
-        bloom_filter = [False] * self.bloom_size
-        
-        for line_number, username in enumerate(usernames, 1): 
-            username_hash = hashlib.sha256(username.encode()).hexdigest()
-            hash_table[username_hash] = line_number
-            for i in range(self.hash_function_count): 
-                index = int(hashlib.sha256(f'{username}{i}'.encode()).hexdigest(), 16) % self.bloom_size
-                bloom_filter[index] = True
-
-        return bloom_filter, hash_table'''
-
 
 
     def linear_search(self, target_username):
@@ -81,37 +66,4 @@ class Algorithm:
 
 
 
-    def bloom_filter_search(self, target_username):
-        start_time = time.time()
 
-        hash_function = 7
-        array_size = 500000
-        [False] * array_size
-        usernames = self.read_file()
-
-        for i in hash_function:
-            hash_table = {}
-            line_number = 0
-            for username in usernames:
-                username_hash = hashlib.sha256(username.encode()).hexdigest() 
-                hash_table[username_hash] = hashlib.sha256(username.encode()).hexdigest() % array_size
-                line_number += 1
-
-        for i in range(hash_function):
-            digest = mmh3.hash(item, i) % self.size
-            digests.append(digest)
-
-            # set the bit True in bit_array
-            self.bit_array[digest] = True
-
-        array_size[hash_table] = True
-        
-
-        for i in range(self.hash_function_count):
-            index = int(hashlib.sha256(f'{target_username}{i}'.encode()).hexdigest(), 16) % self.bloom_size
-            if not self.bloom_filter[index]:
-                return -1, f"{time.time() - start_time:.4f} seconds"
-            
-        username_hash = hashlib.sha256(target_username.encode()).hexdigest()
-        result = self.hash_table.get(username_hash, -1)
-        return result, f"{time.time() - start_time:.4f} seconds"
